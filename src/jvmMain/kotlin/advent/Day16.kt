9960f3a3aaf65d1622a399c7095ad93549dee167
daniel-m-dd <daniel.marin@datadoghq.com>
2019-12-18 18:02:32 +0100

    Day 16 - Draft II

--- src/jvmMain/kotlin/advent/Day16.kt
+++ src/jvmMain/kotlin/advent/Day16.kt
@@ -6,18 +6,18 @@ class Day16 : Day {
 
     override fun execute1() {
         val input = loadFile("day16.txt").first()
-        val res = compute("12345678")
+        val res = compute(input)
         println(res.take(8))
     }
 
     private fun compute(s: String): String {
         return (0 until 100).fold(s) { input: String, _ ->
-            input.mapIndexed { index, _ -> transformInput(input, index, 1) }.joinToString("")
+            input.mapIndexed { index, _ -> transformInput(input, index) }.joinToString("")
                     .also { println(it[0]) }
         }
     }
 
-    private fun transformInput(input: String, row: Int, startOffset: Int): Char {
+    private fun transformInput(input: String, row: Int): Char {
         return input.foldIndexed(0) { col, sum, c ->
             val value = c.toString().toInt()
             sum + value * pattern[((col + 1) / (row + 1)).rem(pattern.size)]
@@ -25,10 +25,27 @@ class Day16 : Day {
     }
 
     override fun execute2() {
-        val msg = loadFile("day16.txt").first()
-        val input = (0 until 10000).joinToString("") { msg }
-        val res = compute(input)
-        println(res)
+        val input = loadFile("day16.txt").first()
+        val msg = (0 until 10000).joinToString("") { input /* "02935109699940807407585447034323" */ }
+        // The rule for the last digit after half the string length is easier since all matrices coefficients == 1
+        require(msg.take(7).toInt() > msg.length / 2)
+        val start = msg.drop(msg.take(7).toInt()).reversed()
+
+        // Recipe is to take the value at position i - 1 of current phase and add it to position i of previous phase
+        // -> Empirical equation: f(phase, index) = f(phase - 1, index) + f(phase, index - 1)
+
+//        Working but not efficient
+        val result = (0 until 100).fold(start) { reversedInput, _ ->
+            println("new phase")
+            // Recipe is to take the value at position i - 1 of current phase and add it to position i of previous phase
+            reversedInput.fold("") { builtString, newChar ->
+                val previouslyBuiltItem = builtString.lastOrNull() ?: return@fold newChar.toString()
+                val newValue = previouslyBuiltItem.toString().toInt() + newChar.toString().toInt()
+                builtString + newValue.toString().last()
+            }
+        }
+
+        println(result.reversed().take(8))
     }
 
 }
--- src/jvmMain/kotlin/advent/Main.kt
+++ src/jvmMain/kotlin/advent/Main.kt
@@ -1,5 +1,5 @@
 package advent
 
 fun main() {
-    Day16().execute1()
+    Day16().execute2()
 }
